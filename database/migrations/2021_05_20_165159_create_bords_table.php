04b7984c61990bba8523c4b599b75c7681a38f76
s-hamada2640 <hsat2640@gmail.com>
2021-05-23 08:15:50 +0900

    データベース再修正

--- database/migrations/2021_05_20_165159_create_bords_table.php
+++ database/migrations/2021_05_20_165159_create_bords_table.php
@@ -15,14 +15,14 @@ class CreateBordsTable extends Migration
     {
         Schema::create('replies', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name', 50)->nullable(false);
-            $table->string('title', 50);
-            $table->string('comment', 300)->nullable(false);
+            $table->string('name', 50);
+            $table->string('title', 50)->nullable();
+            $table->string('comment', 300);
             $table->timestamps();
-            $table->unsignedInteger('big_category_id');
-            $table->foreign('big_category_id')->references('id')->on('big_categories')->nullable(false);
-            $table->unsignedInteger('small_category_id');
-            $table->foreign('small_category_id')->references('id')->on('small_categories')->nullable(false);
+            $table->unsignedInteger('big_categories_id')->unsigned();
+            $table->unsignedInteger('small_categories_id')->unsigned();
+            $table->foreign("big_categories_id")->references("id")->on("big_categories");
+            $table->foreign("small_categories_id")->references("id")->on("small_categories");
         });
     }
 
