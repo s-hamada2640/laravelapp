0bf1713e3d4ea4a25f42044ae3207f4906502edd
hotate1000 <ooo.336.sss@gmail.com>
2021-06-05 23:26:11 +0900

    TopAndCreatePageEdit

--- resources/views/bords/index.blade.php
+++ resources/views/bords/index.blade.php
@@ -12,14 +12,19 @@
 
 @section('main')
   <div class='bords-index'>
-    <div class="bords-index-itemBox">
-    @foreach( $big_category as $k )
-        <a href="/bords/create?bigflg={{ $k->id }}" class="bords-index-itemBox-button">
-          <div class="bords-index-itemBox-item">
-          {{ $k->bigCategory }}
+    <div class="bords-index-main">
+      <div class="bords-index-main-topText">
+        <p>育児について書き込む掲示板です。</br>興味あるカテゴリを選択してください。</p>
+      </div>
+      <div class="bords-index-main-itemBox">
+        @foreach( $big_category as $k )
+        <a href="/bords/create?bigflg={{ $k->id }}" class="bords-index-main-itemBox-button">
+          <div class="bords-index-main-itemBox-item">
+            {{ $k->bigCategory }}
           </div>
         </a>
-      @endforeach
+        @endforeach
+      </div>
     </div>
   </div>
 @endsection
